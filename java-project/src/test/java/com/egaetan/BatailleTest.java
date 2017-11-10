14711b2ea5e47fd252367948afea5d9511306e43
ELEOUET Gaëtan <gaetan.eleouet@gmail.com>
2017-11-10 11:45:55 +0100

    Update BatailleTest.java

--- java-project/src/test/java/com/egaetan/BatailleTest.java
+++ java-project/src/test/java/com/egaetan/BatailleTest.java
@@ -22,7 +22,7 @@ public class BatailleTest {
 		String res = baos.toString().trim();
 		Assert.assertEquals(testName, expected, res);
 		
-		msg("Succès", testName);
+		msg("Succès", ":white_check_mark: "+ testName);
 	}
 	
 	@Test
