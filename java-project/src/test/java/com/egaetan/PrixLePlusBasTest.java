8ba75450eabc4b3ef08eaf67d5becfeffb9e4240
ELEOUET Gaëtan <gaetan.eleouet@gmail.com>
2017-11-11 23:40:55 +0100

    add icons

--- java-project/src/test/java/com/egaetan/PrixLePlusBasTest.java
+++ java-project/src/test/java/com/egaetan/PrixLePlusBasTest.java
@@ -10,15 +10,15 @@ import org.junit.Test;
 public class PrixLePlusBasTest extends AbstractTestRunner {
 
 	public PrixLePlusBasTest() {
-		super(() -> new PrixLePlusBas().main(), "Prix remporté ! € $");
+		super(() -> new PrixLePlusBas().main(), "Prix remporté ! 💶");
 	}
 	
 	@Test
 	public void test() {
-		runTestFromFile("prixLePlusBas/prix1.txt", "Poivre & Sel", "10");
-		runTestFromFile("prixLePlusBas/prix2.txt", "Panier de fruits", "21");
-		runTestFromFile("prixLePlusBas/prix3.txt", "Compote", "11");
-		runTestFromFile("prixLePlusBas/prix4.txt", "Grand salon", "1");
+		runTestFromFile("prixLePlusBas/prix1.txt", "Poivre & Sel 🍴", "10");
+		runTestFromFile("prixLePlusBas/prix2.txt", "Panier de fruits 🍒 🍌🍍", "21");
+		runTestFromFile("prixLePlusBas/prix3.txt", "Compote 🍎", "11");
+		runTestFromFile("prixLePlusBas/prix4.txt", "Grand salon 🏠", "1");
 	}
 
 	@Test
