90a79c5d20cbf3e875283dc37bf0e1c43b3a1c04
ELEOUET Gaëtan <gaetan.eleouet@gmail.com>
2017-11-13 17:12:00 +0100

    Update ADNTest.java

--- java-project/src/test/java/com/egaetan/ADNTest.java
+++ java-project/src/test/java/com/egaetan/ADNTest.java
@@ -10,9 +10,56 @@ public class ADNTest extends AbstractTestRunner {
 	
 	@Test
 	public void test() {
-		runTestFromFile("adn/test1.txt", "Simple", "8");
+		Data data1 = new Data("AT", "G", "CC", "TAG");
+		runTest(reader(() -> input(data1), "Simple", s -> verify(data1.brins, s)); 		
 	}
 
+	static class Data {
+		String[] brins;
+
+		public Data(String... brins) {
+			super();
+			this.brins = brins;
+		}
+		
+		public void verify(String s) {
+			verify(brins, s);
+		}
+	}
+	
+	private String input(String...sol) {
+		return Arrays.stream(sol).collect(Collectors.joining("\n", sol.length + "\n", ""));
+	}
+	
+	private void verify(String[] inputs, String sol) {
+		Map<String, Long> counts = count(Arrays.stream(inputs));
+		
+		Pattern pat = Pattern.compile("#"); 
+		Pattern patSpace = Pattern.compile(" ");		
+		Map<String, Long> solCounts = count(pat.splitAsStream(sol).flatMap(s -> patSpace.splitAsStream(s)));
+		
+		Assert.assertEquals("Different size", counts.size(), solCounts.size());
+		
+		Optional<String> findAny = counts.keySet().stream()
+			.filter(k -> counts.get(k).equals(solCounts.computeIfAbsent(k, s -> 0L)))
+			.findAny();
+		
+		findAny.ifPresent(s -> Assert.fail(s + " count"));
+		
+		String[] split = pat.split(sol);
+		
+		String left = split[0].replaceAll(" ", "");
+		String right = split[1].replaceAll(" ", "")
+				.replaceAll("A", "U").replaceAll("T", "A").replaceAll("U", "T")
+				.replaceAll("G", "U").replaceAll("C", "G").replaceAll("U", "C");
+		
+		Assert.assertEquals(left, right);
+		
+	}
+
+	private Map<String, Long> count(Stream<String> stream) {
+		return stream.collect(Collectors.groupingBy(s -> s, Collectors.counting()));
+	}
 	
 	
 }
