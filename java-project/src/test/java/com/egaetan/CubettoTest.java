6bd449403b93b738aebb5e31d0408ed58f42a08f
ELEOUET Gaëtan <gaetan.eleouet@gmail.com>
2017-11-14 01:13:47 +0100

    start test

--- java-project/src/test/java/com/egaetan/CubettoTest.java
+++ java-project/src/test/java/com/egaetan/CubettoTest.java
@@ -1,5 +1,6 @@
 package com.egaetan;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CubettoTest extends AbstractTestRunner {
@@ -10,9 +11,71 @@ public class CubettoTest extends AbstractTestRunner {
 	
 	@Test
 	public void test() {
-		runTestFromFile("cubetto/test1.txt", "Simple test, A vainqueur", "A");
+		runFromData("Cubetto", new CubettoData(
+				  "......"
+				+ ".>..O."
+				+ "......"
+				+ "......"
+				+ "......"
+				+ "......"
+						  ));
 	}
 
+	static class CubettoData implements Data {
+
+		boolean possible = true;
+		String map;
+		
+		
+		
+		public CubettoData(String map) {
+			super();
+			this.map = map;
+		}
+
+		@Override
+		public void check(String actual) {
+			checkNb(actual);
+			// TODO Auto-generated method stub
+			
+		}
+
+		private void checkNb(String actual) {
+			int verts= 0;
+			int jaunes = 0;
+			int rouges = 0;
+			int bleus = 0;
+			for (int i = 0; i < actual.length(); i++) {
+				switch (actual.charAt(i)) {
+				case 'V':
+					verts ++;
+				case 'R':
+					rouges ++;
+				case 'J':
+					jaunes ++;
+				case 'B':
+					bleus ++;
+					break;
+				case '#':
+					break;
+				default:
+					Assert.fail("'" + actual.charAt(i) + "' innatendu");
+					break;
+				}
+			}
+			Assert.assertTrue("Nombre de verts", verts <= 4);
+			Assert.assertTrue("Nombre de jaunes", jaunes <= 4);
+			Assert.assertTrue("Nombre de rouges", rouges <= 4);
+			Assert.assertTrue("Nombre de bleus", bleus <= 4);
+		}
+
+		@Override
+		public String input() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	}
 	
 	
 }
