7988bb4af07a416b3cbdf0b45468bf12003b2aa0
ELEOUET Gaëtan <gaetan.eleouet@gmail.com>
2017-11-14 01:03:30 +0100

    pre code

--- java-project/src/main/java/com/egaetan/Cubetto.java
+++ java-project/src/main/java/com/egaetan/Cubetto.java
@@ -14,8 +14,34 @@ public class Cubetto {
 	public void main() {
 		String  line;
 		Scanner sc = new Scanner(System.in);
-		while(sc.hasNextLine()) {
+		for (int iy = 0; iy < 6; iy++) {
 			line = sc.nextLine();
+			for (int ix = 0; ix < 6; ix++) {
+				switch (line.charAt(ix)) {
+				case 'X':
+					
+					break;
+				case 'O':
+					
+					break;
+				case 'P':
+					
+					break;
+				case '^':
+					
+				case '<':
+					
+				case 'v':
+					
+				case '>':
+					
+					break;
+
+				case '.':
+				default:
+					break;
+				}
+			}
 			/* Lisez les données et effectuez votre traitement */
 		}
 	/* Vous pouvez aussi effectuer votre traitement une fois que vous avez lu toutes les données.*/
